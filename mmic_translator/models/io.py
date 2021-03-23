8acbf5d97b112212ebf23f201824e00be82869ec
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-23 13:10:46 -0400

    Trans Models inhereit from procedure schemas

--- mmic_translator/models/io.py
+++ mmic_translator/models/io.py
@@ -1,44 +1,39 @@
 from typing import Dict, Any, Optional, List
 from mmelemental.models.proc import ProcInput, ProcOutput
+from mmelemental.models.base import ProtoModel
 from pydantic import Field, root_validator
 
 __all__ = ["TransInput", "TransOutput"]
 
 
-class TransInput(ProcInput):
-    """An input model that serves as an intermediate input object used in converting toolkit data objects
-    to MMSchema models."""
-
-    tk_object: Any = Field(
-        None, 
-        description="Toolkit data object that stores atoms e.g. MDanalysis.Universe object."
+class Trans(ProtoModel):
+    data_object: Any = Field(
+        None,
+        description="Toolkit-specific data object that stores atoms e.g. MDanalysis.Universe object.",
     )
-    tk_units: Optional[Dict[str, str]] = Field(
-        None, description="Units for variables stored in tk_object."
+    data_units: Optional[Dict[str, str]] = Field(
+        None, description="Units for variables stored in data_object."
     )
     schema_object: Any = Field(None, description="MMSchema data object or model.")
 
-
     @root_validator
-    def _valid_fields(cls, values):
-        if values["tk_object"] and values["schema_object"]:
+    def _valid_trans_fields(cls, values):
+        if values["data_object"] and values["schema_object"]:
             raise ValueError(
-                "tk_object and schema_object cannot be simultaneously defined."
+                "data_object and schema_object cannot be simultaneously defined."
             )
         return values
 
 
-class TransOutput(ProcOutput):
+class TransInput(ProcInput, Trans):
+    """An input model that serves as an intermediate input object used in converting toolkit data objects
+    to MMSchema models."""
+
+    ...
+
+
+class TransOutput(ProcOutput, Trans):
     """An output model that serves as an intermediate output object used in converting toolkit data objects
     to MMSchema models."""
 
-    tk_object: Any = Field(
-        None, 
-        description="Toolkit data object that stores atoms e.g. MDanalysis.Universe object."
-    )
-    tk_units: Optional[Dict[str, str]] = Field(
-        None, description="Units for variables stored in tk_object."
-    )
-    schema_object: Any = Field(None, description="MMSchema data object or model.")
-
-
+    trans_input: TransInput
