0be96942e61b347756a29ef0d023499029ff810a
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-05-24 14:42:08 -0400

    lower-case mdanalysis

--- mmic_translator/components/supported.py
+++ mmic_translator/components/supported.py
@@ -1,12 +1,10 @@
 __all__ = ["reg_trans"]
 
 reg_trans = {
-    "mmic_mda": "MDAnalysis",
+    "mmic_mda": "mdanalysis",
     "mmic_parmed": "parmed",
     "mmic_qcschema": "qcschema",
     # "mmic_rdkit": "rdkit",
     # "mmic_gmx": "gmx",
 }
 reg_vers = {}
-
-
