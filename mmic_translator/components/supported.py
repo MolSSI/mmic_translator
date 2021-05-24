61e0a0559bd13fe5262392903602d89d7e1ea6ba
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-05-24 12:08:40 -0400

    add support for qcschema

--- mmic_translator/components/supported.py
+++ mmic_translator/components/supported.py
@@ -3,6 +3,7 @@ __all__ = ["reg_trans"]
 reg_trans = {
     "mmic_mda": "MDAnalysis",
     "mmic_parmed": "parmed",
+    "mmic_qcschema": "qcschema",
     # "mmic_rdkit": "rdkit",
     # "mmic_gmx": "gmx",
 }
