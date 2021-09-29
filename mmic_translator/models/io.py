b48d85749b5169c9fa10856785de1919b4bf9725
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-09-29 13:37:52 -0400

    remove Trans base model; update class names

--- mmic_translator/models/io.py
+++ mmic_translator/models/io.py
@@ -1,12 +1,15 @@
 from typing import Dict, Any, Optional
-from cmselemental.models.procedures import ProcInput, ProcOutput
+from cmselemental.models.procedures import InputProc, OutputProc
 from cmselemental.models.base import ProtoModel
 from pydantic import Field, root_validator
 
-__all__ = ["TransInput", "TransOutput"]
+__all__ = ["InputTrans", "OutputTrans"]
 
 
-class Trans(ProtoModel):
+class InputTrans(InputProc):
+    """An input model that serves as an intermediate input object used in converting toolkit data objects
+    to MMSchema models."""
+
     data_object: Any = Field(
         None,
         description="Toolkit-specific data object that stores atoms e.g. MDanalysis.Universe object.",
@@ -15,6 +18,15 @@ class Trans(ProtoModel):
         None, description="Units for variables stored in data_object."
     )
     schema_object: Any = Field(None, description="MMSchema data object or model.")
+
+    schema_name: str = Field(
+        ...,
+        description=f"The specification to which this model conforms.",
+    )
+    schema_version: int = Field(
+        ...,
+        description="The version number of ``schema_name`` to which this model conforms.",
+    )
 
     @root_validator
     def _valid_trans_fields(cls, values):
@@ -25,24 +37,19 @@ class Trans(ProtoModel):
         return values
 
 
-class TransInput(ProcInput, Trans):
-    """An input model that serves as an intermediate input object used in converting toolkit data objects
-    to MMSchema models."""
-
-    schema_name: str = Field(
-        ...,
-        description=f"The specification to which this model conforms.",
-    )
-    schema_version: int = Field(
-        ...,
-        description="The version number of ``schema_name`` to which this model conforms.",
-    )
-
-
-class TransOutput(ProcOutput, Trans):
+class OutputTrans(OutputProc):
     """An output model that serves as an intermediate output object used in converting toolkit data objects
     to MMSchema models."""
 
+    data_object: Any = Field(
+        None,
+        description="Toolkit-specific data object that stores atoms e.g. MDanalysis.Universe object.",
+    )
+    data_units: Optional[Dict[str, str]] = Field(
+        None, description="Units for variables stored in data_object."
+    )
+    schema_object: Any = Field(None, description="MMSchema data object or model.")
+
     schema_name: str = Field(
         ...,
         description=f"The specification to which this model conforms.",
@@ -51,6 +58,14 @@ class TransOutput(ProcOutput, Trans):
         ...,
         description="The version number of ``schema_name`` to which this model conforms.",
     )
-    proc_input: TransInput = Field(
+    proc_input: InputTrans = Field(
         None, description="Translation procedure input model."
     )
+
+    @root_validator
+    def _valid_trans_fields(cls, values):
+        if values["data_object"] and values["schema_object"]:
+            raise ValueError(
+                "data_object and schema_object cannot be simultaneously defined."
+            )
+        return values
